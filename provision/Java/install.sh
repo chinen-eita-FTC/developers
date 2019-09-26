d6a83d20b0769740435c011799c1f632bf67b4a2
chinen-eita-FTC <e_chinen@ft-cons.co.jp>
2019-09-27 00:28:24 +0900

    Gradleのソースコード配置場所まで移動する処理を追加

--- provision/Java/install.sh
+++ provision/Java/install.sh
@@ -8,8 +8,9 @@ yum install -y  java-1.8.0-openjdk java-1.8.0-openjdk-devel
 echo "【終了】Java 8 (JDK) ランタイムとjava 8 (JDK) 開発環境を取得"
 
 echo "【開始】ビルド管理ツール(=Gradle)を取得、パスを設定"
+cd /usr/local/src
 wget https://services.gradle.org/distributions/gradle-4.4-all.zip
 unzip /usr/local/src/gradle-4.4-all.zip -d /usr/local/src/
-ln -s /usr/local/src/gradle-4.4/bin/gradle /usr/bin/
+ln -s /usr/local/src/gradle-4.4/bin/gradle /usr/bin/gradle
 rm /usr/local/src/gradle-4.4-all.zip
 echo "【終了】ビルド管理ツール(=Gradle)を取得、パスを設定"
